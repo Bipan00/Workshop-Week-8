220b46302ed6f8a04fd41fc0bfc96dd42c57bf48
unknown <bipant45@gmail.com>
2025-12-18 11:31:47 +0545

    Initial commit

---
+++ Components/Pages/Calculator.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop8.Components.Pages
+{
+    public partial class Calculator : ComponentBase
+    {
+        // Two-way bound numbers
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+
+        // Result of calculation
+        public double Result { get; set; }
+
+        // Event handlers
+        public void Add()
+        {
+            Result = Num1 + Num2;
+        }
+
+        public void Subtract()
+        {
+            Result = Num1 - Num2;
+        }
+
+        public void Multiply()
+        {
+            Result = Num1 * Num2;
+        }
+
+        public void Divide()
+        {
+            if (Num2 != 0)
+                Result = Num1 / Num2;
+            else
+                Result = double.NaN; // or display error
+        }
+    }
+}
